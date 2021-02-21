c9ff9553e23c24f78374e529d7af5ffe79a6483e
Edivan de Castro <edivandecastro@gmail.com>
2021-02-21 11:18:20 -0300

    Melhorado a forma como os menus são organizados para serem persistidos

--- src/models/Menu.ts
+++ src/models/Menu.ts
@@ -2,12 +2,14 @@ import MenuSchema from '../schemas/Menu'
 
 class Menu {
   public async build(menu_params) {
-    if(menu_params.parent_id) {
-      let menu = await MenuSchema.findById(menu_params.parent_id)
+
+    if(menu_params.ancestors) {
+      let menu;
+      menu = await MenuSchema.findById(menu_params.ancestors[0])
+      let submenu = new MenuSchema(menu_params)
+      this.addSubmenu(submenu, menu, 1, menu_params.ancestors)
 
       if (menu) {
-        let submenu = new MenuSchema(menu_params);
-        menu.submenus.push(submenu);
         return menu;
       }
       else {
@@ -16,6 +18,17 @@ class Menu {
     }
     else {
       return new MenuSchema(menu_params);
+    }
+  }
+
+  private addSubmenu(submenu, menu, i, ids) {
+    let m = menu.submenus.find(submenu => String(submenu._id) === ids[i])
+    if (i < ids.length) {
+      i += 1
+      this.addSubmenu(submenu, m, i, ids)
+    }
+    else {
+      menu.submenus.push(submenu)
     }
   }
 }
